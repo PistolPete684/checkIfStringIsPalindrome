df78af9182273c6f1401790b6d5b79c7595a7ad6
Peter <pwlinton@mta.ca>
2023-07-30 13:54:57 -0300

    adding js file

---
+++ app.js
@@ -0,0 +1,6 @@
+function isPalindrome(str) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  const cleanedStr = str.replace(/[^a-zA-Z0-9\s]/g, '').toLowerCase();
+  // Compare the cleaned string with its reverse
+  return cleanedStr === cleanedStr.split('').reverse().join('');
+}
